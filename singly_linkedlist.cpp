3b32a32adf81b9dc289a42ce57ca731b6f14ce7a
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-06 11:30:06 +0700

    addbaru1

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -62,4 +62,52 @@ public:
             return (START == NULL);
         }
 
+        bool Search(int nim, Node **previos, Node **current)
+        {
+            *previos = START;
+            *current = START;
+
+            while ((*current != NULL) && (nim != (*current)->noMhs))
+            {
+                *previos = *current;
+                *current = (*Current)->next;
+            }
+
+            return (*current != NULL);
+        }
+
+        bool delNode(int nim)
+        {
+            Node *current, *previous;
+            if (!Search(nim, &previous, &current))
+            return false;
+
+            if (current == START)
+                START = START -> next;
+            else
+                previous->next = current->next;
+            
+            delete current;
+            return true;
+        }
+        void traverse()
+        {
+            if(listEmpty())
+            {
+                cout << "\nList Kosong\n";
+            }
+            else
+            {
+                cout << "\nData didalam list adalah :\n";
+                Node *currentNode = START;
+                while (currentNode != NULL)
+                {
+                    cout << currentNode->noMhs << endl;
+                    currentNode = currentNode->next;
+                }
+
+                cout << endl;
+            }
+        }
+
 };
